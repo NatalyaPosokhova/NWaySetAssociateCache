d6bc49a9923dc565c38c9ed87328348f6951b0f8
Natalya <Natalya.Posokhova@gmail.com>
2020-11-21 20:37:28 +0500

    initial

---
+++ NWaySetAssociateCacheTests/CacheTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using NWaySetAssociateCache;
+
+namespace NWaySetAssociateCacheTests
+{
+    public class CacheTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
