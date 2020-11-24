2db890a349b6c353759d4181d8c7248c3a8778f1
Natalya <Natalya.Posokhova@gmail.com>
2020-11-25 01:01:42 +0500

    add success remove entry test

--- NWaySetAssociateCacheTests/LRUAlgorithmTests.cs
+++ NWaySetAssociateCacheTests/LRUAlgorithmTests.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using NWaySetAssociateCache;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using System;
 
 namespace NWaySetAssociateCacheTests
 {
@@ -43,5 +45,23 @@ namespace NWaySetAssociateCacheTests
             //Assert
             Assert.AreEqual(expectedValue, actualValue);
         }
+
+        [Test]
+        public void RemoveEntryFromCacheShouldBeSuccess()
+        {
+            //Arrange
+            int cacheSize = 255;
+            double key = 6.4;
+            double value = 2.2;
+
+            var algorithm = new LRUAlgorithm<double>(cacheSize);
+            algorithm.Add(key, value);
+            algorithm.Remove(key);
+
+            //Actual
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => algorithm.GetValue(key));
+        }
+
     }
 }
