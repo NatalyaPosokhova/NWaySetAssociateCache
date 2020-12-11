81cc80e47492d67a76883ea154e2c6ab12997458
Natalya <Natalya.Posokhova@gmail.com>
2020-12-11 19:49:01 +0500

    add test for third user algorithm

---
+++ NWaySetAssociateCacheTests/UserAlgorithmTests3.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using NWaySetAssociateCache;
+
+namespace NWaySetAssociateCacheTests
+{
+    [TestFixture]
+    public class UserAlgorithmTests3
+    {
+        private UserAlgorithm<int, string> userAlgorithm;
+
+        [SetUp]
+        public void Setup()
+        {
+            userAlgorithm = new UserAlgorithm<int, string>((key) => UpdateAction.nothing, (key, value) => AddAction.addToFirst, () => RemoveAction.removeFirst);
+        }
+
+        [Test]
+        public void TryAddEntyToUserCacheShouldBeSuccess()
+        {
+            //Arrange
+            int key = 123;
+            string expectedValue = "345";
+
+            userAlgorithm.Add(key, expectedValue);
+
+            //Actual
+            var actualValue = userAlgorithm.GetValue(key);
+
+            //Assert
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+    }
+}
